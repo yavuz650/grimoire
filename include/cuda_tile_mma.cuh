0c5c682a994776e45d1220e0d6306ee37cc53590
Yavuz Tozlu <0yavuz0@gmail.com>
2026-05-31 17:12:45 -0400

    Add layout options

--- include/cuda_tile_mma.cuh
+++ include/cuda_tile_mma.cuh
@@ -1,37 +1,57 @@
 #include "cuda_tile.h"
 #include <cuda_fp16.h>
- 
-/* this kernel multiplies MxK and KxN matrices, where M=8 and N=16.  K is variable but must be divisible by 8.*/
-__tile_global__ void cuda_tile_mma(__half* __restrict__ a, __half* __restrict__ b, float* __restrict__ c, int32_t M, int32_t N, int32_t K) {
-    namespace ct = cuda::tiles;
-    using namespace ct::literals;
- 
-    a = ct::assume_aligned(a, 16_ic);
-    b = ct::assume_aligned(b, 16_ic);
-    c = ct::assume_aligned(c, 16_ic);
- 
-    auto aShape = ct::extents{M, K};
-    auto bShape = ct::extents{K, N};
-    auto cShape = ct::extents{M, N};
 
-    auto aSpan = ct::tensor_span{a, aShape};
-    auto bSpan = ct::tensor_span{b, bShape};
-    auto cSpan = ct::tensor_span{c, cShape};
+#include "common.cuh"
  
-    auto aView = ct::partition_view{aSpan, ct::shape{4_ic, 8_ic}};
-    auto bView = ct::partition_view{bSpan, ct::shape{8_ic, 4_ic}};
-    auto cView = ct::partition_view{cSpan, ct::shape{4_ic, 4_ic}};
-     
-    using f32x4x4 = ct::tile<float, ct::shape<4, 4>>;
-    auto accTile = ct::full<f32x4x4>(0);
+template <Layout LayoutA, Layout LayoutB, Layout LayoutC>
+__tile_global__ void cuda_tile_mma(__half* __restrict__ a, __half* __restrict__ b, float* __restrict__ c, int32_t M, int32_t N, int32_t K) {
+  namespace ct = cuda::tiles;
+  using namespace ct::literals;
  
-    auto [xBlock, yBlock, dummy] = ct::bid();
-    for (auto idx : ct::irange(0, K/8)) {
-        auto aTile = aView.load_masked(yBlock, idx);
-        auto bTile = bView.load_masked(idx, xBlock);
-        accTile = ct::mma(aTile, bTile, accTile);
-    }
+  a = ct::assume_aligned(a, 16_ic);
+  b = ct::assume_aligned(b, 16_ic);
+  c = ct::assume_aligned(c, 16_ic);
  
-    cView.store_masked(accTile, yBlock, xBlock);
+  auto aShape = ct::extents{M, K};
+  auto bShape = ct::extents{K, N};
+  auto cShape = ct::extents{M, N};
+
+  // Construct spans immediately using lambdas
+  auto aSpan = [&] {
+    if constexpr (LayoutA == Layout::RowMajor) 
+      return ct::tensor_span{a, aShape, ct::layout_right{}};
+    else                                       
+      return ct::tensor_span{a, aShape, ct::layout_left{}};
+  }();
+
+  auto bSpan = [&] {
+    if constexpr (LayoutB == Layout::RowMajor) 
+      return ct::tensor_span{b, bShape, ct::layout_right{}};
+    else                                       
+      return ct::tensor_span{b, bShape, ct::layout_left{}};
+  }();
+
+  auto cSpan = [&] {
+    if constexpr (LayoutC == Layout::RowMajor) 
+      return ct::tensor_span{c, cShape, ct::layout_right{}};
+    else                                       
+      return ct::tensor_span{c, cShape, ct::layout_left{}};
+  }();
+ 
+  auto aView = ct::partition_view{aSpan, ct::shape{4_ic, 8_ic}};
+  auto bView = ct::partition_view{bSpan, ct::shape{8_ic, 4_ic}};
+  auto cView = ct::partition_view{cSpan, ct::shape{4_ic, 4_ic}};
+   
+  using f32x4x4 = ct::tile<float, ct::shape<4, 4>>;
+  auto accTile = ct::full<f32x4x4>(0);
+ 
+  auto [xBlock, yBlock, dummy] = ct::bid();
+  for (auto idx : ct::irange(0, K/8)) {
+      auto aTile = aView.load_masked(yBlock, idx);
+      auto bTile = bView.load_masked(idx, xBlock);
+      accTile = ct::mma(aTile, bTile, accTile);
+  }
+ 
+  cView.store_masked(accTile, yBlock, xBlock);
 }
 
--- tests/cuda_tile_mma.cu
+++ tests/cuda_tile_mma.cu
@@ -1,6 +1,7 @@
 #include <ctime>
 #include <cuda_fp16.h>
 
+#include "cutlass/layout/matrix.h"
 #include "include/cuda_tile_mma.cuh"
 #include "include/buffer.cuh"
 #include "include/utils.hpp"
@@ -10,14 +11,58 @@
 
 #include <cutlass/util/host_tensor.h>
 
-int main(int argc, char* argv[]) {
+bool parseLayout(const char* arg, Layout& layout) { 
+  if (arg[0] == 'T' || arg[0] == 't') { 
+    layout = Layout::RowMajor; 
+    return true; 
+  } 
+  if (arg[0] == 'N' || arg[0] == 'n') { 
+    layout = Layout::ColMajor; 
+    return true; 
+  } 
+  return false; 
+}
 
-  if (argc != 4) {
-    fprintf(stderr, "Usage: %s M N K\n", argv[0]);
+// CUTLASS GEMM template
+using ElementOutput = float;
+using ElementAccumulator = float;
+template <typename LayoutA, typename LayoutB, typename LayoutC>
+using GemmKernel = cutlass::gemm::device::Gemm<
+                   cutlass::half_t, LayoutA,
+                   cutlass::half_t, LayoutB,
+                   float, LayoutC,
+                   float,
+                   cutlass::arch::OpClassTensorOp, cutlass::arch::Sm80,
+                   cutlass::gemm::GemmShape<64, 64, 32>,
+                   cutlass::gemm::GemmShape<32, 32, 32>,
+                   cutlass::gemm::GemmShape<16, 8, 16>,
+                   cutlass::epilogue::thread::LinearCombination<
+                       ElementOutput, 128 / cutlass::sizeof_bits<ElementOutput>::value,
+                       ElementAccumulator, ElementAccumulator>,
+                   cutlass::gemm::threadblock::GemmIdentityThreadblockSwizzle<>, 10>;
+
+template <typename LayoutA, typename LayoutB, typename LayoutC>
+cutlass::Status launch_gemm(cutlass::half_t* ptrA, cutlass::half_t* ptrB, float* ptrC, float* ptrD,
+                            int M, int N, int K, float alpha, float beta) {
+  GemmKernel<LayoutA, LayoutB, LayoutC> gemm_op;
+
+  // Dynamically calculate leading dimensions based on layout types
+  int lda = std::is_same_v<LayoutA, cutlass::layout::RowMajor> ? K : M;
+  int ldb = std::is_same_v<LayoutB, cutlass::layout::RowMajor> ? N : K;
+  int ldc = std::is_same_v<LayoutC, cutlass::layout::RowMajor> ? N : M;
+  int ldd = ldc;
+
+  return gemm_op({{M, N, K}, {ptrA, lda}, {ptrB, ldb}, {ptrC, ldc}, {ptrD, ldd}, {alpha, beta}});
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 7) {
+    fprintf(stderr, "Usage: %s M N K LayoutA LayoutB LayoutC\n", argv[0]);
     return 1;
   }
 
-  uint64_t M, N, K;
+  int32_t M, N, K;
+  Layout layoutA, layoutB, layoutC;
   try {
     M = std::stoull(argv[1]);
     N = std::stoull(argv[2]);
@@ -27,6 +72,18 @@ int main(int argc, char* argv[]) {
     return 1;
   } catch (const std::out_of_range&) {
     fprintf(stderr, "Error: M, N, K value out of range\n");
+    return 1;
+  }
+  if (!parseLayout(argv[4], layoutA)) {
+    std::cerr << "Invalid LayoutA: must be 'T' (row-major) or 'N' (column-major)\n";
+    return 1;
+  }
+  if (!parseLayout(argv[5], layoutB)) {
+    std::cerr << "Invalid LayoutB: must be 'T' (row-major) or 'N' (column-major)\n";
+    return 1;
+  }
+  if (!parseLayout(argv[6], layoutC)) {
+    std::cerr << "Invalid LayoutC: must be 'T' (row-major) or 'N' (column-major)\n";
     return 1;
   }
   
@@ -54,55 +111,63 @@ int main(int argc, char* argv[]) {
   // Copy matrices to device
   A_buffer.copyToDevice();
   B_buffer.copyToDevice();
-
-  cuda_tile_mma<<<dim3(N/4, M/4), 1>>>(static_cast<__half*>(A_buffer.getDevicePtr()), static_cast<__half*>(B_buffer.getDevicePtr()),  static_cast<float*>(C0_buffer.getDevicePtr()), M, N, K);
+  if (layoutA == Layout::RowMajor && layoutB == Layout::RowMajor) {
+    cuda_tile_mma<Layout::RowMajor, Layout::RowMajor, Layout::RowMajor><<<dim3(N/4, M/4), 1>>>(static_cast<__half*>(A_buffer.getDevicePtr()), 
+                                                                                               static_cast<__half*>(B_buffer.getDevicePtr()),  
+                                                                                               static_cast<float*>(C0_buffer.getDevicePtr()), M, N, K);
+  } 
+  else if (layoutA == Layout::RowMajor && layoutB == Layout::ColMajor) {
+    cuda_tile_mma<Layout::RowMajor, Layout::ColMajor, Layout::RowMajor><<<dim3(N/4, M/4), 1>>>(static_cast<__half*>(A_buffer.getDevicePtr()), 
+                                                                                               static_cast<__half*>(B_buffer.getDevicePtr()),  
+                                                                                               static_cast<float*>(C0_buffer.getDevicePtr()), M, N, K);
+  } 
+  else if (layoutA == Layout::ColMajor && layoutB == Layout::ColMajor) {
+    cuda_tile_mma<Layout::ColMajor, Layout::ColMajor, Layout::RowMajor><<<dim3(N/4, M/4), 1>>>(static_cast<__half*>(A_buffer.getDevicePtr()), 
+                                                                                               static_cast<__half*>(B_buffer.getDevicePtr()),  
+                                                                                               static_cast<float*>(C0_buffer.getDevicePtr()), M, N, K);
+  } 
+  else if (layoutA == Layout::ColMajor && layoutB == Layout::RowMajor) {
+    cuda_tile_mma<Layout::ColMajor, Layout::RowMajor, Layout::RowMajor><<<dim3(N/4, M/4), 1>>>(static_cast<__half*>(A_buffer.getDevicePtr()), 
+                                                                                               static_cast<__half*>(B_buffer.getDevicePtr()),  
+                                                                                               static_cast<float*>(C0_buffer.getDevicePtr()), M, N, K);
+  } 
   
   CHECK_CUDA_ERROR(cudaGetLastError());
   CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   C0_buffer.copyToHost();
-  using ElementOutput = float;
-  using ElementAccumulator = float;
-  using Gemm = cutlass::gemm::device::Gemm<
-      cutlass::half_t, 
-      cutlass::layout::RowMajor, 
-      cutlass::half_t,
-      cutlass::layout::RowMajor, 
-      float, 
-      cutlass::layout::RowMajor,
-      float, 
-      cutlass::arch::OpClassTensorOp, cutlass::arch::Sm80,
-      cutlass::gemm::GemmShape<64, 64, 32>,
-      cutlass::gemm::GemmShape<32, 32, 32>, 
-      cutlass::gemm::GemmShape<16, 8, 16>,
-      cutlass::epilogue::thread::LinearCombination<
-          ElementOutput, 128 / cutlass::sizeof_bits<ElementOutput>::value,
-          ElementAccumulator, ElementAccumulator>,
-      cutlass::gemm::threadblock::GemmIdentityThreadblockSwizzle<>, 10>;
-
-  Gemm gemm_op;
-  cutlass::Status status;
 
   float alpha = 1.f;
   float beta = 0;
-
   cutlass::half_t* ptrA = static_cast<cutlass::half_t*>(A_buffer.getDevicePtr());
   cutlass::half_t* ptrB = static_cast<cutlass::half_t*>(B_buffer.getDevicePtr());
   float* ptrC = static_cast<float*>(C1_buffer.getDevicePtr());
   float* ptrD = static_cast<float*>(C1_buffer.getDevicePtr());
 
-  int lda = K;
-  int ldb = N;
-  int ldc = N;
-  int ldd = N;
-  // Launch GEMM on the device
-  status = gemm_op({
-    {static_cast<int32_t>(M), static_cast<int32_t>(N), static_cast<int32_t>(K)},
-    {ptrA, lda},            // TensorRef to A device tensor
-    {ptrB, ldb},            // TensorRef to B device tensor
-    {ptrC, ldc},            // TensorRef to C device tensor
-    {ptrD, ldd},            // TensorRef to D device tensor - may be the same as C
-    {alpha, beta}           // epilogue operation arguments
-  });
+  cutlass::Status status;
+  if (layoutA == Layout::RowMajor && layoutB == Layout::RowMajor && layoutC == Layout::RowMajor) {
+    status = launch_gemm<cutlass::layout::RowMajor, cutlass::layout::RowMajor, cutlass::layout::RowMajor>(ptrA, ptrB, ptrC, ptrD, M, N, K, alpha, beta);
+  } 
+  else if (layoutA == Layout::RowMajor && layoutB == Layout::RowMajor && layoutC == Layout::ColMajor) {
+    status = launch_gemm<cutlass::layout::RowMajor, cutlass::layout::RowMajor, cutlass::layout::ColumnMajor>(ptrA, ptrB, ptrC, ptrD, M, N, K, alpha, beta);
+  } 
+  else if (layoutA == Layout::RowMajor && layoutB == Layout::ColMajor && layoutC == Layout::RowMajor) {
+    status = launch_gemm<cutlass::layout::RowMajor, cutlass::layout::ColumnMajor, cutlass::layout::RowMajor>(ptrA, ptrB, ptrC, ptrD, M, N, K, alpha, beta);
+  } 
+  else if (layoutA == Layout::RowMajor && layoutB == Layout::ColMajor && layoutC == Layout::ColMajor) {
+    status = launch_gemm<cutlass::layout::RowMajor, cutlass::layout::ColumnMajor, cutlass::layout::ColumnMajor>(ptrA, ptrB, ptrC, ptrD, M, N, K, alpha, beta);
+  } 
+  else if (layoutA == Layout::ColMajor && layoutB == Layout::RowMajor && layoutC == Layout::RowMajor) {
+    status = launch_gemm<cutlass::layout::ColumnMajor, cutlass::layout::RowMajor, cutlass::layout::RowMajor>(ptrA, ptrB, ptrC, ptrD, M, N, K, alpha, beta);
+  } 
+  else if (layoutA == Layout::ColMajor && layoutB == Layout::RowMajor && layoutC == Layout::ColMajor) {
+    status = launch_gemm<cutlass::layout::ColumnMajor, cutlass::layout::RowMajor, cutlass::layout::ColumnMajor>(ptrA, ptrB, ptrC, ptrD, M, N, K, alpha, beta);
+  } 
+  else if (layoutA == Layout::ColMajor && layoutB == Layout::ColMajor && layoutC == Layout::RowMajor) {
+    status = launch_gemm<cutlass::layout::ColumnMajor, cutlass::layout::ColumnMajor, cutlass::layout::RowMajor>(ptrA, ptrB, ptrC, ptrD, M, N, K, alpha, beta);
+  } 
+  else if (layoutA == Layout::ColMajor && layoutB == Layout::ColMajor && layoutC == Layout::ColMajor) {
+    status = launch_gemm<cutlass::layout::ColumnMajor, cutlass::layout::ColumnMajor, cutlass::layout::ColumnMajor>(ptrA, ptrB, ptrC, ptrD, M, N, K, alpha, beta);
+  }
 
   if (status != cutlass::Status::kSuccess) {
     return -1;
